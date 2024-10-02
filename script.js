b4297158cf570421e4323c45e143ad64736b33e3
Navneet_thakur <127828104+navneet451@users.noreply.github.com>
2024-10-02 11:31:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 //your JS code here. If required.
-cy.get('button').first().click().then(() => {
-    cy.get('.modal').should('be.visible');
-    cy.get('.btn-close').click().then(() => {
-        cy.get('.modal').should('not.be.visible');
-    });
-});
+cy.get('button').first().click();
+cy.get('.modal').should('be.visible');
+cy.wait(500);  // Wait for modal to be fully rendered
+cy.get('.btn-close').click();
+cy.get('.modal').should('not.be.visible');
